46a5f8138eb2e99608a41c4287e83c6677682fb9
Patrick Van den Bergh <patrick@ansapa.com>
2022-03-26 15:29:04 +0100

    Initial Commit

---
+++ Shared/SloxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SloxApp.swift
+//  Shared
+//
+//  Created by Patrick Van den Bergh on 26/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct SloxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
